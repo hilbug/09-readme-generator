0945f9a4b2ef0b6ab5c7eb7f988c1ba904f766b4
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-20 00:13:27 -0400

    index.js start questions, init, writefile

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-// array of questions for user
-const questions = [
+// Requires...
+// Read/write files
+const fs = require("fs");
+// Util.promisify
+const util = require("util");
+// Question prompts
+const inquirer = require("inquirer");
+// Generate markdown function
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
+// Array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter a title for your project.'
+    }
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+// Prompt User with questions
+let promptUser = () => {
+    return inquirer.prompt(questions);
 }
 
-// function to initialize program
-function init() {
+// function to write README file
+// function writeToFile(fileName, data) {
+// }
+const writeToFile = util.promisify(fs.writeFile);
 
+// function to initialize program - 9-3 ex. 40
+let init = async () => {
+    console.log("initializing");
+    try {
+        // Get user answers
+        const answers = await promptUser();
+        console.log(answers);
+
+        // Write answers to readme
+        const readme = generateMarkdown(answers);
+
+        // Save readme to a file
+        await writeToFile("README_test.md", readme);
+
+        // File successful
+        console.log("Successfully wrote to README_test.md");
+
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
