3fb003b194032eeff98775b9d28700f595c11ed4
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-20 03:59:07 -0400

    add markdown text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
 
+## Description
+${data.description}  
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+${data.installation} 
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Contact email: ${data.email}\n
+GitHub Profile: [${data.github}](https://github.com/${data.github})
+${data.addlquestions}
+
+## License
+This project is licensed under the terms of the ${data.license.toUpperCase()} license.
 `;
 }
 
@@ -10,8 +39,9 @@ module.exports = generateMarkdown;
 
 /* README Format from Good-README-Guide
 Title
-License badge 
+License badge
   https://shields.io/category/license
+  https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 Description
 Table of contents (Links)
 Sections:
