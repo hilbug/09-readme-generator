93daeb04d7fb6ab0590795eab64c6052be9b1af8
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-20 00:44:51 -0400

    generatemd add pseudocode

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+/* README Format from Good-README-Guide
+Title
+License badge 
+  https://shields.io/category/license
+Description
+Table of contents (Links)
+Sections:
+  Installation
+  Usage
+  Contributing
+  Tests
+  Questions
+    Email
+    GitHub Profile link
+  License
+*/
+
